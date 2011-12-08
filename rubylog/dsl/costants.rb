ea4bd555dddd48282d3b4509e48b586d1e7ddb54
Kalló Bernát <kallo.bernat@gmail.com>
2011-12-08 11:57:33 +0100

    work on constants

--- rubylog/dsl/costants.rb
+++ rubylog/dsl/costants.rb
@@ -1,8 +1,14 @@
 module Rubylog
   module DSL
     module Constants
-      def const_missing c
+      def self.included class_or_module
+        class_or_module.extend ClassMethods
+      end
 
+      module ClassMethods
+        def const_missing c
+          Rubylog::Variable.new c
+        end
       end
     end
   end
