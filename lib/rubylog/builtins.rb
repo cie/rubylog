59c8aaa30ea28aa244174447e79fd3a338c50aa8
Kalló Bernát <kallo.bernat@gmail.com>
2012-09-27 22:10:44 +0200

    removed some unnecessary builtins

--- lib/rubylog/builtins.rb
+++ lib/rubylog/builtins.rb
@@ -12,51 +12,53 @@ module Rubylog
 
     # prolog builtins
 
+    # true
     def true
       yield
     end
 
+    # fail
     def fail
     end
 
+    # ,
     def and a, b
       a.prove { b.prove { yield } }
     end
 
+    # !
     def cut
       yield
       raise Cut
     end
 
+    # ;
     def or a, b
       a.prove { yield }
       b.prove { yield }
     end
 
-    def then a,b
+    # ->
+    def in_which_case a,b:not, 
       stands = false
       a.prove { stands = true ; break }
       b.prove { yield } if stands
     end
 
-    def is_false a
+    # not \+
+    def false a
       a.prove { return }
       yield
     end
 
-    def repeat
-      yield while true
-    end
-
     # unification
 
+    # = is
     def is a,b
       a = a.rubylog_resolve_function
       b = b.rubylog_resolve_function
       a.rubylog_unify(b) { yield }
     end
-
-    
 
     def matches a,b
       a = a.rubylog_resolve_function
@@ -64,7 +66,8 @@ module Rubylog
       yield if b.rubylog_dereference === a.rubylog_dereference
     end
 
-    def splits_to a,h,t
+    # A=[H|T]
+    def list a,h,t
       t = t.rubylog_dereference
       if t.instance_of? Rubylog::Variable
         a = a.rubylog_dereference
@@ -88,6 +91,7 @@ module Rubylog
       end
     end
 
+    # member
     def in a,b
       a = a.rubylog_resolve_function
       b = b.rubylog_resolve_function.rubylog_dereference
@@ -106,25 +110,9 @@ module Rubylog
       end
     end
 
-    def _p a
-      p a.rubylog_deep_dereference
-      yield
-    end
 
-    def _puts a
-      puts a.rubylog_deep_dereference
-      yield
-    end
 
-    def _print a
-      print a.rubylog_deep_dereference
-      yield
-    end
-
-    def _nl
-      puts
-      yield
-    end
+    # ruby iterator predicate allegories
 
     def all a,b
       a.prove {
@@ -180,14 +168,11 @@ module Rubylog
   # aliases
   BUILTINS[:&][2] = BUILTINS[:and][2]
   BUILTINS[:|][2] = BUILTINS[:or][2]
-  BUILTINS[:~][1] = 
-    BUILTINS[:not][1] =
-    BUILTINS[:none][1] =
-    BUILTINS[:fails][1] = BUILTINS[:is_false][1]
-  BUILTINS[:false][0] = BUILTINS[:fail][0]
+  BUILTINS[:~][1] = BUILTINS[:none][1] = BUILTINS[:false][1]
+  BUILTINS[:fail][0]
 
-  DSL.add_first_order_functor :is, :matches, :in, :_p, :_puts, :_print
-  DSL.add_second_order_functor :and, :or, :then, :is_false, :&, :|, :~, :not, :all, :any, :one, :none, :fails
-  DSL.add_functors_to Array, :splits_to
+  DSL.add_first_order_functor :is, :matches, :in
+  DSL.add_second_order_functor :and, :or, :in_which_case, :false, :&, :|, :~, :all, :any, :one, :none
+  DSL.add_functors_to Array, :list
 
 end
