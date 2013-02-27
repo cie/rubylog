ccc3150585513f20df7f43126f7c625f16988e40
Kalló Bernát <kallo.bernat@gmail.com>
2013-02-27 23:07:31 +0100

    fixed a test broken by {X==nil}

--- logic/map_logic.rb
+++ logic/map_logic.rb
@@ -8,7 +8,7 @@ theory "Rubylog::MapLogic" do
   check G.is(:fail.or :fail).and L.is{G.map{X}}.and{L == []}
   check S.is{X.is(1).or(X.is(1)).map{X}}.and{S == [1,1]}
   check [1,2].is{X.is(2).or(X.is(1)).map{X}}.false
-  check [X,Y].is{X.is(1).or(X.is(2)).map{X}}.and{X == 1}.and{Y == 2}
+  check [X,Y].is{A.is(1).or(A.is(2)).map{A}}.and{X == 1}.and{Y == 2}
   (S.is {Goal.map{X}}).solve and check :fail rescue NoMethodError
   (S.is {4.map{X}}).solve and check :fail rescue NoMethodError
 end
