f762031ba04dee55d5f74993a74cb3caaf07838e
Kalló Bernát <kallo.bernat@gmail.com>
2013-02-27 23:17:19 +0100

    not showing . and .. in dir_in and dirname_in

--- lib/rubylog/builtins/file_system.rb
+++ lib/rubylog/builtins/file_system.rb
@@ -4,8 +4,8 @@ theory "Rubylog::FileSystemBuiltins", nil do
   
   X.filename_in(D).if Y.file_in(D).and X.is { File.basename Y }
   X.dirname_in(D).if  Y.dir_in(D).and  X.is { File.basename Y }
-  X.file_in(D).if Y.in { d = D.rubylog_resolve_function; Dir["#{d}/*", "#{d}/.*"].sort }.and { not File.directory? Y }.and X.is {File.expand_path Y}
-  X.dir_in(D). if Y.in { d = D.rubylog_resolve_function; Dir["#{d}/*", "#{d}/.*"].sort }.and {     File.directory? Y }.and X.is {File.expand_path Y}
+  X.file_in(D).if Y.in { d = D.rubylog_resolve_function; Dir["#{d}/*", "#{d}/.*"].sort }.and { not ['.','..'].include? File.basename(Y) and not File.directory? Y }.and X.is {File.expand_path Y}
+  X.dir_in(D). if Y.in { d = D.rubylog_resolve_function; Dir["#{d}/*", "#{d}/.*"].sort }.and { not ['.','..'].include? File.basename(Y) and     File.directory? Y }.and X.is {File.expand_path Y}
 
 end
 
