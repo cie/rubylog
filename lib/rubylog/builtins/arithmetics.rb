7db5f44b11c4689b133e36edac5cbc659f2c9767
Kalló Bernát <kallo.bernat@gmail.com>
2013-03-10 08:57:06 +0100

    refactoring arithmetics

--- lib/rubylog/builtins/arithmetics.rb
+++ lib/rubylog/builtins/arithmetics.rb
@@ -8,11 +8,7 @@ Rubylog.theory "Rubylog::ArithmeticsBuiltins", nil do
 
   class << primitives
     def is c, a, op, b
-      a = a.rubylog_resolve_function.rubylog_dereference
-      b = b.rubylog_resolve_function.rubylog_dereference
-      c = c.rubylog_resolve_function.rubylog_dereference
-      op=op.rubylog_resolve_function.rubylog_dereference
-
+      a, b, c, op = [a,b,c,op].map{|f|f.rubylog_resolve_function.rubylog_dereference}
       a_var, b_var, c_var, op_var = [a,b,c,op].map{|f|f.is_a? Rubylog::Variable}
 
       raise Rubylog::InstantiationError, [:is, c, a, op, b] if op_var
