25eb2d5b05c7c7aceeadf7be5ffdc41b4e50e7a1
Kalló Bernát <kallo.bernat@gmail.com>
2013-04-06 09:40:15 +0200

    removed .is(,,)

--- lib/rubylog/builtins/arithmetics.rb
+++ lib/rubylog/builtins/arithmetics.rb
@@ -1,46 +1,23 @@
 Rubylog::DefaultBuiltins.amend do
 
-  Rubylog::OperatorInverses = {
-    :+ => [lambda{|c,b|c-b}, lambda{|c,a|c-a}],
-    :- => [lambda{|c,b|c+b}, lambda{|c,a|a-c}]
-  }
-
-
-  class << primitives_for Rubylog::Term
-    def is c, a, op, b
-      a, b, c, op = [a,b,c,op].map{|f|f.rubylog_resolve_function.rubylog_dereference}
-      a_var, b_var, c_var, op_var = [a,b,c,op].map{|f|f.is_a? Rubylog::Variable}
-
-      raise Rubylog::InstantiationError.new :is, [c, a, op, b] if op_var
+  class << primitives_for Numeric
+    def sum_of c, a, b
+      a, b, c = [a,b,c].map{|f|f.rubylog_resolve_function.rubylog_dereference}
+      a_var, b_var, c_var = [a,b,c].map{|f|f.is_a? Rubylog::Variable}
 
       case
       when !a_var && !b_var
-        c.rubylog_unify a.send(op,b) do
-          yield
-        end
+        c.rubylog_unify(a+b) { yield }
       when !c_var && !b_var
-        inv_op = Rubylog::OperatorInverses[op][0]
-        result = inv_op[c, b]
-        a.rubylog_unify result do
-          yield
-        end
+        a.rubylog_unify(c-b) { yield }
       when !c_var && !a_var
-        inv_op = Rubylog::OperatorInverses[op][1]
-        result = inv_op[c, a]
-        b.rubylog_unify result do
-          yield
-        end
+        b.rubylog_unify(c-a) { yield }
       else
-        raise Rubylog::InstantiationError.new :is, [c, a, op, b]
+        raise Rubylog::InstantiationError.new :sum_of, [c, a, b]
       end
+
     end
 
-    def is_not c,a,op,b
-      self.is(c,a,op,b) do
-        return
-      end
-      yield
-    end
   end
 end
 
