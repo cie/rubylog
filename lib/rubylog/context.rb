c1ad1583b51fecee5fc4dd9c41af35a6a341156b
Kalló Bernát <kallo.bernat@gmail.com>
2013-04-22 09:03:30 +0200

    removed implicit mode

--- lib/rubylog/context.rb
+++ lib/rubylog/context.rb
@@ -5,7 +5,6 @@ require 'rubylog/context_modules/builtins'
 require 'rubylog/context_modules/checks'
 require 'rubylog/context_modules/demonstration'
 require 'rubylog/context_modules/dynamic_mode'
-require 'rubylog/context_modules/implicit'
 require 'rubylog/context_modules/inclusion'
 require 'rubylog/context_modules/predicates'
 require 'rubylog/context_modules/primitives'
@@ -25,7 +24,6 @@ module Rubylog::Context
   include Rubylog::ContextModules::Checks
   include Rubylog::ContextModules::Demonstration
   include Rubylog::ContextModules::DynamicMode
-  include Rubylog::ContextModules::Implicit
   include Rubylog::ContextModules::Inclusion
   include Rubylog::ContextModules::Predicates
   include Rubylog::ContextModules::Primitives
--- lib/rubylog/context_modules/static_mode.rb
+++ lib/rubylog/context_modules/static_mode.rb
@@ -1,9 +1,7 @@
 module Rubylog::ContextModules
   module StaticMode
     def amend &block
-      with_implicit do
-        return instance_exec &block
-      end
+      return instance_exec &block
     end
 
     alias query amend
