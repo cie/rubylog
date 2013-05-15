377880ee02f6b2ffe7e002fde49f01d03d8b2a5b
Kalló Bernát <kallo.bernat@gmail.com>
2013-05-15 07:56:23 +0200

    moved recursion spec to unit tests

--- spec/rubylog/variable_spec.rb
+++ spec/rubylog/variable_spec.rb
@@ -145,6 +145,20 @@ describe Rubylog::Variable, :rubylog=>true do
     check 3.is(Rubylog::Variable.new(:var1 )).and(4.is(Rubylog::Variable.new(:var1 ))).false
   end
 
+  describe "variables support recursion" do
+    predicate_for Integer, ".factorial()"
+    0.factorial! 1
+    N[thats > 0].factorial(K).if N1.is {N-1} .and N1.factorial(K1).and K.is{ N*K1 }
+
+    check 0.factorial 1
+    check 1.factorial 1
+    check 2.factorial 2
+    check 3.factorial 6
+    check 4.factorial 24
+    check 7.factorial 5040
+  end
+
+
   describe "dont-care variables support recursion" do
     predicate_for Integer, ".factorial()"
     0.factorial! 1
