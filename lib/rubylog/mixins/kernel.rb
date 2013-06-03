981a85d76f329c3ac5504b5f98867ee0299b58a1
Kalló Bernát <kallo.bernat@gmail.com>
2013-06-03 13:02:55 +0200

    added executable and load_rubylog_file

--- lib/rubylog/mixins/kernel.rb
+++ lib/rubylog/mixins/kernel.rb
@@ -3,4 +3,12 @@ module Kernel
   def rubylog &block
     Rubylog::DefaultContext.class_exec &block
   end
+
+  # requires a Rublog file in a Rubylog context
+  def load_rubylog_file filename, context=Rubylog::DefaultContext
+    require "rubylog/rubylog_files"
+    source = File.read(filename)
+    source = Rubylog::RubylogFiles.convert_source(source)
+    eval(source, TOPLEVEL_BINDING, filename, 1)
+  end 
 end
